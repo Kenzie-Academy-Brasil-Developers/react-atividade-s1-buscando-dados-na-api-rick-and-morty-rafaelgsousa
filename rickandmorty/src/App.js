47a8f57ca6fda2a3795ed79945c953051d24fbe4
rafaeldev <elderrafaelgomes@gmail.com>
2021-09-09 01:15:01 -0300

    Criação de componentes, requisição em uma API e stilização

---
+++ rickandmorty/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+import { useState, useEffect } from 'react';
+
+import Characters from './componentes/Characters/inde';
+
+
+function App() {
+
+  const [caracterList, setCaracterList] = useState([])
+
+  useEffect(()=>{
+    fetch("https://rickandmortyapi.com/api/character")
+    .then((response)=>response.json())
+    .then((response)=>setCaracterList(response.results))
+    .catch(((err)=>console.log(err)))
+  },[])
+
+  console.log(caracterList)
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Characters personagens={caracterList}/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
